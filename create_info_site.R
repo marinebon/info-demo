0ac1623d7e32bce35d7a0370b51b5a3dac72d834
Tylar Murray <murray.tylar@gmail.com>
2017-08-24 17:24:50 +0000

    + #!shabang!

--- create_info_site.R
+++ create_info_site.R
@@ -1,2 +1,8 @@
+#!/usr/bin/env Rscript
+#
+# run w/ Rscript like:
+# # Rscript create_info_site.R
+
+# this script assumes you have already installed the infographiq package
 # devtools::install_github('marinebon/infographiq')
 infographiq::create_info_site(render_modals = F)
